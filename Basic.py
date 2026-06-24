50ca1de911e644c4bb6623c1fbf3de7e22795523
piyush-y0804 <piyushyadav0804@gmail.com>
2026-06-25 00:49:17 +0530

    Add option feature
    Added an option to ask for whether the user wants to add income, expense or see financial report.

--- Basic.py
+++ Basic.py
@@ -2,12 +2,25 @@ income = 0
 expense = 0
 
 def TakeInputIncome():
-    return int(input("What is your income: "))
+    return int(input("What is your income: \n"))
 
 def TakeInputExpense():
-    return int(input("What are the expenses(amount): "))
+    return int(input("What are the expenses(amount): \n"))
 
-income = TakeInputIncome()
-expense = TakeInputExpense()
-print("your income is: ", income)
-print("your expense is: ", expense)
+def TakeOption():
+    return input("\n \nWhat would you like to do: \n 1.Add Income \n 2.Add Expense \n 3.See Financial Report \n Please choose a number: ")
+
+while True:
+    option = TakeOption()
+    print()
+    if option == "1":
+        income = TakeInputIncome()
+        print("Income added successfully!")
+    elif option == "2":
+        expense = TakeInputExpense()
+        print("Expense added successfully!")
+    elif option == "3":
+        print("your income is: ", income)
+        print("your expense is: ", expense, "\n")
+    else:
+        print("Please select a valid number! \n")
